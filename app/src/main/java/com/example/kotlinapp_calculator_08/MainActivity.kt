2ed0a4b6e5a5d9e061c2848858ae3b525c92abca
JorgeRC <jorgercdeveloper@gmail.com>
2023-02-09 15:56:23 +0000

    Initial commit

---
+++ app/src/main/java/com/example/kotlinapp_calculator_08/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinapp_calculator_08
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
